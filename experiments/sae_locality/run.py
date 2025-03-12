bb8ac2a727dadc28c5f958931c17fca5b7e5a9dd
melembroucarlitos <melembroucarlitos@gmail.com>
2025-03-12 00:29:02 -0700

    outline experiment main

--- experiments/sae_locality/run.py
+++ experiments/sae_locality/run.py
@@ -1,7 +1,12 @@
 import fire
 
-def foo():
-    print("Hello World")
+def main():
+    raise NotImplementedError
+    # Train ground truth model
+    # Run ENTK on ground truth model
+    # Train SAEs on ground truth model
+    # Compare Frobenius Norm
+    # ?? Shuffle Control ??
 
 if __name__ == "__main__":
-    fire.Fire(foo)
+    fire.Fire(main)
